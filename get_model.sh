40500d24c960d01b937be49eb85730b0cfed5699
nic.nguyen <nguyenhoangnguyen96nt@gmail.com>
2023-12-03 17:36:59 +0700

    Demo ready

---
+++ get_model.sh
@@ -0,0 +1,2 @@
+wget -O ./server/model.zip https://www.dropbox.com/scl/fi/h7mwahk9n9bx03f8rxer1/model_repository.zip\?rlkey\=oi0k98k23qyr3uys9cpfpit92\&dl\=0
+unzip ./server/model.zip -d ./server
---
+++ server/nvidia_convert_onnx_code/update_text_recognition_weight.py
@@ -0,0 +1,14 @@
+import torch
+from server.utils.model import STRModel
+
+# Create PyTorch Model Object
+model = STRModel(input_channels=1, output_channels=512, num_classes=37)
+
+# Load model weights from external file
+state = torch.load("None-ResNet-None-CTC.pth")
+state = {key.replace("module.", ""): value for key, value in state.items()}
+model.load_state_dict(state)
+
+# Create ONNX file by tracing model
+trace_input = torch.randn(1, 1, 32, 100)
+torch.onnx.export(model, trace_input, "str.onnx", verbose=True, dynamic_axes={'input.1':[0],'308':[0]})
